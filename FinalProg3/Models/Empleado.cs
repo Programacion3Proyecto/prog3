4f03a567e5318f123c80f9cdcb2686c0301c29f0
Gold3nDarkness <zlmasters2@gmail.com>
2018-12-02 03:58:04 -0400

    AddEmpleados

---
+++ FinalProg3/Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProg3.Models
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public int nombre { get; set; }
+        public int apellido { get; set; }
+        public int puesto { get; set; }
+        public int usuario { get; set; }
+        public int clave { get; set; }
+
+    }
+}
